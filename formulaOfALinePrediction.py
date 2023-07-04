68fe1ea18e5f4faa812b95b4e087e01e2cb895a2
Rohit Nair <87155954+rohitNair21@users.noreply.github.com>
2023-07-03 18:02:16 -0600

    Use an x and y value array to predict a formula

---
+++ formulaOfALinePrediction.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Sequential 
+from tensorflow.keras.layers import Dense
+
+model = Sequential([Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+   
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+model.fit(xs, ys, epochs=500) 
+print(model.predict([20.0]))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
